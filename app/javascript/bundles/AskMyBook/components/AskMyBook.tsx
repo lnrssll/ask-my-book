2bf4e89d1981438698daac93a9b05bdd5a9eb2ca
lnrssll <lnrssll@pm.me>
2023-05-07 12:03:09 -0700

    some bug fixes to get typescript working

--- app/javascript/bundles/AskMyBook/components/AskMyBook.tsx
+++ app/javascript/bundles/AskMyBook/components/AskMyBook.tsx
@@ -1,31 +1,14 @@
-import PropTypes from "prop-types";
-import React, { useState } from "react";
+import React from "react";
 import style from "./AskMyBook.module.css";
 
-const AskMyBook = (props) => {
-  const [name, setName] = useState(props.name);
-
+const AskMyBook = () => {
+  const val: string = "AskMyBook";
   return (
-    <div>
-      <h3>Hello, {name}!</h3>
+    <div className={style.temp}>
+      <h3>{val}!</h3>
       <hr />
-      <form>
-        <label htmlFor="name">
-          Say hello to:
-          <input
-            id="name"
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-      </form>
     </div>
   );
-};
-
-AskMyBook.propTypes = {
-  name: PropTypes.string.isRequired, // this is passed from the Rails view
 };
 
 export default AskMyBook;
--- app/javascript/packs/ask-my-book-bundle.js
+++ app/javascript/packs/ask-my-book-bundle.js
@@ -1,6 +1,7 @@
 import ReactOnRails from "react-on-rails";
 
 import App from "../bundles/AskMyBook/components/App";
+import "../stylesheets/AskMyBook.css";
 
 // This is how react_on_rails can see the AskMyBook in the browser.
 ReactOnRails.register({
